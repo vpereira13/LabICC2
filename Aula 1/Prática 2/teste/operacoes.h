25f73cb5fcbae3969465217a243783c39fd0f895
Victor Luiz <victor.smp13@gmail.com>
2018-08-28 21:02:14 -0300

    Limpando repositório

--- Aula 1/Prática 2/teste/operacoes.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OPERACOES_H_
-#define _OPERACOES_H_
-
-void soma(int *, int *, int *);
-void subtracao(int *, int *, int *);
-//int * multiplicacao(int *, int *, int *);
-//int *divisao(int *, int *);
-
-#endif
