8afe0b01debfe91c2787a536094bd0f081c5b53d
Victor Luiz <victor.smp13@gmail.com>
2018-09-21 13:09:49 -0300

    Primeiro commit da aula 5, arquivos base

---
+++ Aula 5/pratica6.c
@@ -0,0 +1,16 @@
+/**
+ * Laboratório de Introdução à Ciência da Computação II - SCC0220
+ *
+ * Prof: André Carlos Ponce de Leon Ferreira de Carvalho
+ *
+ * Aula 4
+ * Exercício 1 - Campeonato desordenado
+ * Victor Luiz da Silva Mariano Pereira	8602444
+ */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+
+	return 0;
+}
