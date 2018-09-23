4cf362c60e28d19a80608e0f526dfb01cac54986
Victor Luiz <victor.smp13@gmail.com>
2018-09-23 16:37:49 -0300

    Etapa de coleta de dados feita

--- Aula 5/pratica6.c
+++ Aula 5/pratica6.c
@@ -9,8 +9,45 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void imprimeTela(char **tela, int x){
+	int i;
+	for(i = 0; i < x; i++)
+		printf("%s\n", tela[i]);
+}
 
 int main (int argc, char *argv[]){
+	int i;
+	int j;
+	int x;
+	int y;
+	int coordX;
+	int coordY;
+	char tinta;
+	char lixo;
+	char **tela;
+
+	scanf("%d %d\n", &x, &y);
+
+	// Alocando memória da tela
+	tela = (char **) malloc (sizeof(char *) * x);
+	for(i = 0; i < x; i++)
+		tela[i] = (char *) malloc (sizeof(char) * y);
+
+	// Recebendo o conteúdo da tela
+	for(i = 0; i < x; i++){
+		for(j = 0; j < y; j++)
+			scanf("%c", &tela[i][j]);
+		scanf("%c", &lixo);
+	}
+
+	// Recebendo a posição e a tinta a ser preenchida na tela
+	while(scanf("%d %d %c\n", &coordX, &coordY, &tinta) != EOF){
+		// pegar a lista dos adjacentes e fazer a coloração
+		imprimeTela(tela, x);
+	}
 
 	return 0;
 }
