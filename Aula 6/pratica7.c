53722a164922c221c7c745f4701c3fc58b9a8c37
Victor Luiz <victor.smp13@gmail.com>
2018-09-28 14:22:10 -0300

    Primeiro commit, adição dos arquivos base

---
+++ Aula 6/pratica7.c
@@ -0,0 +1,16 @@
+/**
+ * Laboratório de Introdução à Ciência da Computação II - SCC0220
+ *
+ * Prof: André Carlos Ponce de Leon Ferreira de Carvalho
+ *
+ * Aula 6
+ * Exercício 1 - Lista Backward
+ * Victor Luiz da Silva Mariano Pereira	8602444
+ */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+
+	return 0;
+}
