63a31175ce1a52a5342a7f382c44dce6e29e30aa
Victor Luiz <victor.smp13@gmail.com>
2018-10-01 22:22:40 -0300

    Adição de um esboço do programa

--- Aula 6/pratica7.c
+++ Aula 6/pratica7.c
@@ -9,8 +9,39 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "listaligada.h"
+
+
+/*
+	Estrutura utilizada
+		Lista ligada com:
+			Chave
+			ponteiro para o “back”
+			e ponteiro para o próximo
+ */
 
 int main (int argc, char *argv[]){
+	int n;
+	int back;
+	char operacao;
+	Lista *L = criaLista();
+	Item *I = NULL;
+
+	// Coletando entrada
+	while(scanf("%c %d %d\n", &operacao, &n, &back) > 1 && operacao != 'f'){
+		if(operacao == 'i'){
+			I = criaItem(n, back);
+			insereFim(L, I);
+			imprimeLista(L);
+		}
+		else if(operacao == 'r')
+			removeItem(L, n);
+	}
+
+	imprimeLista(L);
+	esvaziaLista(L);
+	free(L);
 
 	return 0;
 }
