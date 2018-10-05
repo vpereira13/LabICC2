b74d4b79d965100db5fff6fcc92345930e36f1d8
Victor Luiz <victor.smp13@gmail.com>
2018-10-05 13:54:28 -0300

    Primeiro commit, parte dos arquivos base

---
+++ Aula 7/pratica8.c
@@ -0,0 +1,17 @@
+/**
+ * Laboratório de Introdução à Ciência da Computação II - SCC0220
+ *
+ * Prof: André Carlos Ponce de Leon Ferreira de Carvalho
+ *
+ * Aula 7
+ * Exercício 1 -
+ * Victor Luiz da Silva Mariano Pereira	8602444
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+
+	return 0;
+}
