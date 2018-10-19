c25ab6b7fa47d847cf1dd990d7cbd12d81d4797d
Victor Luiz <victor.smp13@gmail.com>
2018-10-19 18:24:09 -0300

    Terminada a lógica, falta verificar vazamento de memória

--- Aula 8/pratica9.c
+++ Aula 8/pratica9.c
@@ -10,25 +10,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "fila.h"
 
 int main (int argc, char *argv[]){
 	int i;
 	int ncasos;
 	int carta;
+	Item *I;
+	Fila *baralho = criaFila();
+	Fila *mesa = criaFila();
 
 	// Pegando número de casos
 	scanf("%d\n", &ncasos);
 
 	// Para cada caso
 	for(i = 0; i < ncasos; i++){
-		// Coleta as cartas e coloca na estrutura
+		// Coleta as cartas e coloca no baralho
 		scanf("%d", &carta);
 		while(carta){
-			// Adicionar carta na estrutura
+			I = criaItem(carta);
+			insere(baralho, I);
+
 			scanf("%d", &carta);
 		}
 		// Roda o algoritmo de descarte e reposição
+		while(tamanhoFila(baralho) != 1){
+			I = retira(baralho);
+			insere(mesa, I);
+			I = retira(baralho);
+			insere(baralho, I);
+		}
+		printf("Cartas jogadas: ");
+		imprimeFila(mesa);
+		printf("Cart restante: %d\n", valor(I));
+
+		esvaziaFila(baralho);
+		esvaziaFila(mesa);
 	}
 
+	free(baralho);
+	free(mesa);
+	free(I);
+	
 	return 0;
 }
