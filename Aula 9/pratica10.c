d66f735e62c387e319810e1b24c35079857d98ea
Victor Luiz <victor.smp13@gmail.com>
2018-10-26 14:12:04 -0300

    Primeiros arquivos base

---
+++ Aula 9/pratica10.c
@@ -0,0 +1,17 @@
+/**
+ * Laboratório de Introdução à Ciência da Computação II - SCC0220
+ *
+ * Prof: André Carlos Ponce de Leon Ferreira de Carvalho
+ *
+ * Aula 9
+ * Exercício 1 - Cartas
+ * Victor Luiz da Silva Mariano Pereira	8602444
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+
+	return 0;
+}
