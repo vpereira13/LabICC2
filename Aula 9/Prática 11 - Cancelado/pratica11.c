436313358408c32b570ebe5bea9bdc6fc926d506
Victor Luiz <victor.smp13@gmail.com>
2018-10-29 18:39:41 -0300

    Alterações na estrutura das tarefas por conta de cancelamento de exercícios

--- Aula 9/Prática 11 - Cancelado/pratica11.c
+++ Aula 9/Prática 11 - Cancelado/pratica11.c
@@ -4,7 +4,7 @@
  * Prof: André Carlos Ponce de Leon Ferreira de Carvalho
  *
  * Aula 9
- * Exercício 2 - Tabela Hash <>
+ * Exercício 2 - Tabela Hash
  * Victor Luiz da Silva Mariano Pereira	8602444
  */
 
