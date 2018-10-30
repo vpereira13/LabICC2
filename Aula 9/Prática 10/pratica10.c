dfc15b1dd7cb2b70c00e6776e7d06f67b42687a2
Victor Luiz <victor.smp13@gmail.com>
2018-10-30 19:48:09 -0300

    Visão geral do algoritmo

--- Aula 9/Prática 10/pratica10.c
+++ Aula 9/Prática 10/pratica10.c
@@ -12,6 +12,23 @@
 #include <stdlib.h>
 
 int main (int argc, char *argv[]){
+	int i;
+	int ncasos;
+	char *str_arvore1;
+	char *str_arvore2;
+
+	scanf("%d\n", &ncasos);
+
+	str_arvore1 = (char *) malloc (sizeof(char) * 1024);
+	str_arvore2 = (char *) malloc (sizeof(char) * 1024);
+
+	for(i = 0; i < ncasos; i++){
+		scanf("%s\n", str_arvore1);
+		scanf("%s\n", str_arvore2);
+	}
+
+	free(str_arvore1);
+	free(str_arvore2);
 
 	return 0;
 }
