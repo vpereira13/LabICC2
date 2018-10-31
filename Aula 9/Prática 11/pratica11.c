9907397a0dbfc2b1c7def4c81585224ad062280a
Victor Luiz <victor.smp13@gmail.com>
2018-10-30 23:44:44 -0300

    Terminada a implementação do trabalho

--- Aula 9/Prática 11/pratica11.c
+++ Aula 9/Prática 11/pratica11.c
@@ -10,8 +10,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "hash.h"
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]){
+	int resultado;
+	char operacao;
+	char *palavra;
+
+	// Cria a tabela hash
+	Hash *H = cria_hash(365);
+
+	// Aloca tamanho para a palavra
+	palavra = (char *) malloc(sizeof(char) * 30);
+
+	scanf("%c %s\n", &operacao, palavra);
+
+	while(operacao != 'f'){
+		// Analisa entrada
+		switch (operacao){
+			// Caso de insercao
+			case 'i':
+				insere(H, palavra);
+			break;
+			// Caso de busca
+			case 'b':
+				resultado = busca(H, palavra);
+				resultado ? printf("encontrado\n") : printf("nao encontrado\n");
+			break;
+			// Caso de remocao
+			case 'r':
+				remove_item(H, palavra);
+			break;
+		}
+
+		// Pegando a próxima entrada
+		scanf("%c %s\n", &operacao, palavra);
+	}
+
+	// Liberando memória
+	free(palavra);
+	esvazia_hash(H);
+	free(H);
 
 	return 0;
 }
