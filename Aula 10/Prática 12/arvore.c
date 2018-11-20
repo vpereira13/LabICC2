7711355e5a476618649c1e1bd31f7200255230f1
Victor Luiz da Silva Mariano Pereira <victor.luiz.pereira@usp.br>
2018-11-20 09:45:40 -0200

    Implementada a inserção e nova forma do valor

--- Aula 10/Prática 12/arvore.c
+++ Aula 10/Prática 12/arvore.c
@@ -12,23 +12,48 @@
 #include <stdlib.h>
 #include "arvore.h"
 
+struct item{
+	int conteudo;
+};
+
 struct arvore{
-	int valor;
+	Item *i;
 	struct arvore *direita;
 	struct arvore *esquerda;
 };
 
+Item *criaItem(int valor){
+	Item *I = (Item *) malloc(sizeof(Item));
+	I->conteudo = valor;
+
+	return I;
+}
+
 Arvore *criaArvore(){
 	Arvore *A = (Arvore *) malloc(sizeof(Arvore));
 
+	A->i = NULL;
 	A->direita = NULL;
 	A->esquerda = NULL;
 
 	return A;
 }
 
-void insereItem(Arvore *A, int valor){
-
+void insereItem(Arvore *A, int valor, int *erro){
+	if(!A)
+		A = criaArvore();
+	if(!(A->i)){
+		A->i = criaItem(valor);
+		*erro = 0;
+	}
+	else{
+		if(A->i->conteudo > valor)
+			insereItem(A->esquerda, valor, erro);
+		else if(A->i->conteudo < valor)
+			insereItem(A->direita, valor, erro);
+		else
+			*erro = 1;
+	}
 }
 
 void removeItem(Arvore *A, int valor){
@@ -39,9 +64,9 @@ int buscaItem(Arvore *A, int valor){
 	if(!A)
 		return 0;
 	else{
-		if(A->valor == valor)
+		if(A->i->conteudo == valor)
 			return 1;
-		return valor > A->valor ? buscaItem(A->direita, valor) : buscaItem(A->esquerda, valor);
+		return valor > A->i->conteudo ? buscaItem(A->direita, valor) : buscaItem(A->esquerda, valor);
 	}
 }
 
@@ -50,7 +75,7 @@ void imprimeIN(Arvore *A){
 		return;
 	else{
 		imprimeIN(A->esquerda);
-		printf(" %d", A->valor);
+		printf(" %d", A->i->conteudo);
 		imprimeIN(A->direita);
 	}
 }
@@ -58,7 +83,7 @@ void imprimePRE(Arvore *A){
 	if(!A)
 		return;
 	else{
-		printf(" %d", A->valor);
+		printf(" %d", A->i->conteudo);
 		imprimeIN(A->esquerda);
 		imprimeIN(A->direita);
 	}
@@ -69,7 +94,7 @@ void imprimePOS(Arvore *A){
 	else{
 		imprimeIN(A->esquerda);
 		imprimeIN(A->direita);
-		printf(" %d", A->valor);
+		printf(" %d", A->i->conteudo);
 	}
 }
 void imprimeLargura(Arvore *A){
@@ -78,10 +103,21 @@ void imprimeLargura(Arvore *A){
 }
 
 void imprimeTodos(Arvore *A){
+	printf("InOrdem:");
 	imprimeIN(A);
+	printf("\n");
+
+	printf("PreOrdem:");
 	imprimePRE(A);
+	printf("\n");
+
+	printf("PosOrdem:");
 	imprimePOS(A);
+	printf("\n");
+
+	printf("Largura:");
 	imprimeLargura(A);
+	printf("\n");
 }
 
 void esvazia(Arvore *A){
