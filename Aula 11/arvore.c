9f9a3202765b46a3e92bb5f203afb963d293555b
Victor Luiz <victor.smp13@gmail.com>
2018-11-26 20:23:44 -0200

    Implementadas as funções de criação da árvore, iniciação de um nó e impressão para debug

--- Aula 11/arvore.c
+++ Aula 11/arvore.c
@@ -8,3 +8,41 @@
  * Victor Luiz da Silva Mariano Pereira 8602444
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+
+struct arvore{
+    int valor;
+    char flag;
+    struct arvore *direita;
+    struct arvore *esquerda;
+};
+
+Arvore *iniciaArvore(){
+    Arvore *A = (Arvore *) malloc(sizeof(Arvore));
+    A->flag = 'f';
+    A->direita = NULL;
+    A->esquerda = NULL;
+
+    return A;
+}
+
+Arvore *criaArvore(int valor, int altura){
+    Arvore *A = iniciaArvore();
+    A->valor = valor;
+    if(valor * 2 < (altura * altura) - 1){
+        A->esquerda = criaArvore(valor * 2, altura);
+        A->direita = criaArvore(valor * 2 + 1, altura);
+    }
+
+    return A;
+}
+
+void imprimeArvore(Arvore *A){
+    if(A){
+        printf("%d\t%c\n", A->valor, A->flag);
+        imprimeArvore(A->esquerda);
+        imprimeArvore(A->direita);
+    }
+}
