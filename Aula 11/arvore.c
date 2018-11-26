f0b1e0c40e3848cccc3ad699ac70898297ddf99b
Victor Luiz <victor.smp13@gmail.com>
2018-11-26 20:38:07 -0200

    Adicionado os comentários dos códigos feitos até agora

--- Aula 11/arvore.c
+++ Aula 11/arvore.c
@@ -12,6 +12,11 @@
 #include <stdlib.h>
 #include "arvore.h"
 
+/**
+ * Estrutura de árvore binária com o valor do nó, a flag dele se está como
+ * 'v' ou 'f', um ponteiro para o filho da esquerda e um para o filho da
+ * direita
+ */
 struct arvore{
     int valor;
     char flag;
@@ -19,6 +24,12 @@ struct arvore{
     struct arvore *esquerda;
 };
 
+/**
+ * Função que aloca espaço na memória para a criação da árvore, setando seus
+ * filhos como null e a flag como false
+ *
+ * @retval Um nó de uma árvore com seus filhos null e com a flag false
+ */
 Arvore *iniciaArvore(){
     Arvore *A = (Arvore *) malloc(sizeof(Arvore));
     A->flag = 'f';
@@ -28,6 +39,15 @@ Arvore *iniciaArvore(){
     return A;
 }
 
+/**
+ * Função que cria uma árvore binária completa dada uma altura, com os valores
+ * dos nós e todas as flags setadas como falso
+ *
+ * @param  valor: Valor que o nó raiz dessa árvore vai receber
+ * @param  altura: Altura da árvore final para limitar os filhos
+ *
+ * @retval Uma árvore completa limitada pela altura dada
+ */
 Arvore *criaArvore(int valor, int altura){
     Arvore *A = iniciaArvore();
     A->valor = valor;
@@ -39,6 +59,11 @@ Arvore *criaArvore(int valor, int altura){
     return A;
 }
 
+/**
+ * Função que imprime uma dada árvore da forma pré ordem
+ *
+ * @param  *A: Árvore a ser impressa
+ */
 void imprimeArvore(Arvore *A){
     if(A){
         printf("%d\t%c\n", A->valor, A->flag);
