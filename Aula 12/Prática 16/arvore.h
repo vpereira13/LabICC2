a31d6a3f1a37f7d91353ba8fcff1690eaad11513
Victor Luiz <victor.smp13@gmail.com>
2018-12-03 16:50:46 -0200

    Declaração dos cabeçalhos das funções

---
+++ Aula 12/Prática 16/arvore.h
@@ -0,0 +1,25 @@
+/**
+ * Laboratório de Introdução à Ciência da Computação II - SCC0220
+ *
+ * Prof: André Carlos Ponce de Leon Ferreira de Carvalho
+ *
+ * Aula 12
+ * Exercício 2 - Editor de Árvore AVL II
+ * Victor Luiz da Silva Mariano Pereira	8602444
+ */
+
+#ifndef _ARVORE_H_
+#define _ARVORE_H_
+
+typedef struct arvore Arvore;
+
+Arvore *criaArvore();
+int insere(Arvore *, int);
+
+void imprimeIN(Arvore *);
+void imprimePRE(Arvore *);
+void imprimePOS(Arvore *);
+void imprimeLargura(Arvore *);
+void imprimeTodos(Arvore *);
+
+#endif
