46af6a24e5abc34a649b8bf298e6dec8ab3215dd
Victor Luiz <victor.smp13@gmail.com>
2018-12-04 02:21:46 -0200

    Reimplementada a função de inserir na árvore e implementada a impressão em largura

--- Aula 10/Prática 12/arvore.c
+++ Aula 10/Prática 12/arvore.c
@@ -5,55 +5,39 @@
  *
  * Aula 10
  * Exercício 1 - Editor de Árvore Binária de Busca
- * Victor Luiz da Silva Mariano Pereira	8602444
+ * Victor Luiz da Silva Mariano Pereira    8602444
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "arvore.h"
 
-struct item{
-	int conteudo;
-};
-
 struct arvore{
-	Item *i;
-	struct arvore *direita;
-	struct arvore *esquerda;
+    int valor;
+    struct arvore *direita;
+    struct arvore *esquerda;
 };
 
-Item *criaItem(int valor){
-	Item *I = (Item *) malloc(sizeof(Item));
-	I->conteudo = valor;
+Arvore *criaArvore(int valor){
+    Arvore *A = (Arvore *) malloc(sizeof(Arvore));
 
-	return I;
+    A->valor = valor;
+    A->direita = NULL;
+    A->esquerda = NULL;
+
+    return A;
 }
 
-Arvore *criaArvore(){
-	Arvore *A = (Arvore *) malloc(sizeof(Arvore));
-
-	A->i = NULL;
-	A->direita = NULL;
-	A->esquerda = NULL;
-
-	return A;
-}
-
-void insereItem(Arvore *A, int valor, int *erro){
-	if(!A)
-		A = criaArvore();
-	if(!(A->i)){
-		A->i = criaItem(valor);
-		*erro = 0;
-	}
-	else{
-		if(A->i->conteudo > valor)
-			insereItem(A->esquerda, valor, erro);
-		else if(A->i->conteudo < valor)
-			insereItem(A->direita, valor, erro);
-		else
-			*erro = 1;
-	}
+Arvore *insereItem(Arvore *A, int valor, int *erro){
+    if(!A)
+        return criaArvore(valor);
+    if(A->valor > valor)
+        A->esquerda = insereItem(A->esquerda, valor, erro);
+    else if(A->valor < valor)
+        A->direita = insereItem(A->direita, valor, erro);
+    else
+        *erro = 1;
+    return A;
 }
 
 void removeItem(Arvore *A, int valor){
@@ -61,69 +45,101 @@ void removeItem(Arvore *A, int valor){
 }
 
 int buscaItem(Arvore *A, int valor){
-	if(!A)
-		return 0;
-	else{
-		if(A->i->conteudo == valor)
-			return 1;
-		return (valor > A->i->conteudo ? buscaItem(A->direita, valor) : buscaItem(A->esquerda, valor));
-	}
+    if(!A)
+        return 0;
+    else{
+        if(A->valor == valor)
+            return 1;
+        return (valor > A->valor ? buscaItem(A->direita, valor) : buscaItem(A->esquerda, valor));
+    }
 }
 
 void imprimeIN(Arvore *A){
-	if(!A)
-		return;
-	else{
-		imprimeIN(A->esquerda);
-		printf(" %d", A->i->conteudo);
-		imprimeIN(A->direita);
-	}
+    if(!A)
+        return;
+    else{
+        imprimeIN(A->esquerda);
+        printf(" %d", A->valor);
+        imprimeIN(A->direita);
+    }
 }
+
 void imprimePRE(Arvore *A){
-	if(!A)
-		return;
-	else{
-		printf(" %d", A->i->conteudo);
-		imprimeIN(A->esquerda);
-		imprimeIN(A->direita);
-	}
+    if(!A)
+        return;
+    else{
+        printf(" %d", A->valor);
+        imprimeIN(A->esquerda);
+        imprimeIN(A->direita);
+    }
 }
+
 void imprimePOS(Arvore *A){
-	if(!A)
-		return;
-	else{
-		imprimeIN(A->esquerda);
-		imprimeIN(A->direita);
-		printf(" %d", A->i->conteudo);
-	}
+    if(!A)
+        return;
+    else{
+        imprimeIN(A->esquerda);
+        imprimeIN(A->direita);
+        printf(" %d", A->valor);
+    }
 }
+
+int alturaArvore(Arvore *A){
+    int direita;
+    int esquerda;
+
+    if(!A)
+        return 0;
+    else{
+        direita = alturaArvore(A->direita);
+        esquerda = alturaArvore(A->esquerda);
+        return esquerda > direita ? esquerda + 1 : direita + 1;
+    }
+}
+
+void imprimeNivel(Arvore *A, int nivel){
+    if(!A)
+        return;
+    if(nivel == 1)
+        printf(" %d", A->valor);
+    else if(nivel > 1){
+        imprimeNivel(A->esquerda, nivel - 1);
+        imprimeNivel(A->direita, nivel - 1);
+    }
+}
+
 void imprimeLargura(Arvore *A){
-	if(!A)
-		return;
+    int i;
+    int altura;
+
+    altura = alturaArvore(A);
+
+    for(i = 0; i < altura; i++)
+        imprimeNivel(A, i);
 }
 
 void imprimeTodos(Arvore *A){
-	printf("InOrdem:");
-	imprimeIN(A);
-	printf("\n");
+    printf("InOrdem:");
+    imprimeIN(A);
+    printf("\n");
 
-	printf("PreOrdem:");
-	imprimePRE(A);
-	printf("\n");
+    printf("PreOrdem:");
+    imprimePRE(A);
+    printf("\n");
 
-	printf("PosOrdem:");
-	imprimePOS(A);
-	printf("\n");
+    printf("PosOrdem:");
+    imprimePOS(A);
+    printf("\n");
 
-	printf("Largura:");
-	imprimeLargura(A);
-	printf("\n");
+    printf("Largura:");
+    imprimeLargura(A);
+    printf("\n");
 }
 
 void esvazia(Arvore *A){
-	if(!A){
-		esvazia(A->esquerda);
-		esvazia(A->direita);
-		free(A);
-	}
+    if(A){
+        esvazia(A->esquerda);
+        esvazia(A->direita);
+        free(A);
+    }
 }
