da780cdb3cb47ebe2a6a7a68167c4788924b4841
Victor Luiz <victor.smp13@gmail.com>
2018-12-04 14:00:30 -0200

    Modificações para adequar ao exercício

--- Aula 12/Prática 15/pratica15.c
+++ Aula 12/Prática 15/pratica15.c
@@ -5,7 +5,7 @@
  *
  * Aula 12
  * Exercício 1 - Editor de Árvore AVL I
- * Victor Luiz da Silva Mariano Pereira	8602444
+ * Victor Luiz da Silva Mariano Pereira    8602444
  */
 
 #include <stdio.h>
@@ -13,66 +13,65 @@
 #include "arvore.h"
 
 int main (int argc, char *argv[]){
-	int chave;
-	int erro;
-	char instrucao;
+    int chave;
+    int erro;
+    char instrucao;
+    Arvore *A = NULL;
 
-	Arvore *A = criaArvore();
+    scanf("%c", &instrucao);
 
-	scanf("%c", &instrucao);
+    while(instrucao != 'X'){
+        erro = 0;
+        switch(instrucao){
+            // Inserção
+            case 'I':
+                scanf("%d\n", &chave);
+                A = insere(A, chave, &erro);
+                if(erro)
+                    printf("Chave existente\n");
+                break;
 
-	while(instrucao != 'X'){
-		switch(instrucao){
-			// Inserção
-			case 'I':
-				scanf("%d\n", &chave);
-				insereItem(A, chave, &erro);
-				if(erro)
-					printf("Chave existente\n");
-				break;
+            // Impressão In Ordem
+            case 'N':
+                printf("InOrdem:");
+                imprimeIN(A);
+                printf("\n");
+                break;
 
-			// Impressão In Ordem
-			case 'N':
-				printf("InOrdem:");
-				imprimeIN(A);
-				printf("\n");
-				break;
+            // Impressão Pre Ordem
+            case 'E':
+                printf("PreOrdem:");
+                imprimePRE(A);
+                printf("\n");
+                break;
 
-			// Impressão Pre Ordem
-			case 'E':
-				printf("PreOrdem:");
-				imprimePRE(A);
-				printf("\n");
-				break;
+            // Impressão Pos Ordem
+            case 'O':
+                printf("PosOrdem:");
+                imprimePOS(A);
+                printf("\n");
+                break;
 
-			// Impressão Pos Ordem
-			case 'O':
-				printf("PosOrdem:");
-				imprimePOS(A);
-				printf("\n");
-				break;
+            // Impressão Largura
+            case 'L':
+                printf("Largura:");
+                imprimeLargura(A);
+                printf("\n");
+                break;
 
-			// Impressão Largura
-			case 'L':
-				printf("Largura:");
-				imprimeLargura(A);
-				printf("\n");
-				break;
+            // Impressão de todas as formas
+            case 'Y':
+                imprimeTodos(A);
+                break;
 
-			// Impressão de todas as formas
-			case 'Y':
-				imprimeTodos(A);
-				break;
+            default:
+                break;
+        }
+        scanf("%c", &instrucao);
+    }
 
-			default:
-				break;
-		}
-		scanf("%c", &instrucao);
-	}
+    // Liberando memória
+    esvazia(A);
 
-	// Liberando memória
-	esvazia(A);
-	free(A);
-
-	return 0;
+    return 0;
 }
